549ffd5ce32eff1fbb3527dab180488903681c11
Sarwat Afreen <160401550+sarwatafreen@users.noreply.github.com>
2024-04-03 17:23:45 +0500

    my frist commit

---
+++ main.ts
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+
+let todos=[];
+
+let condition = true;
+
+while(condition)
+{
+    let addTask = await inquirer.prompt(
+    [
+        {
+            name:'todos',
+            type:'input',
+            message:"What you want to add in your Todos?"
+        },
+           {
+            name:'addMore',
+            type:'confirm',
+            message:"Do you want to add more ?",
+            default:"false"
+           }
+        ]
+);
+todos.push(addTask.todo);
+condition = addTask.addMore
+console.log(todos)
+//console.log(addTask.todo)
+    }
